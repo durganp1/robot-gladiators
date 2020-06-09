533aa02e52d4faaad33c38e5dba292cc038d3509
durganp1 <durganp1@gmail.com>
2020-06-09 12:38:16 -0500

    add for loop to include enemy robots

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,33 +1,52 @@
+// Game States
+// "WIN" - Player robot has defeated all enemy robots
+//  * Fight all enemy robots
+//  * Defeat each enemy robot
+// "LOSE" - Player robot's health is zero or less
+
+
+
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-console.log(playerName, playerAttack, playerHealth);
+//console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+ //   enemyNames[0];
+ //   enemyNames[1];
+ //   enemyNames[2];
+//console.log(enemyNames);
+//console.log(enemyNames[0]);
+//console.log(enemyNames[1]);
+//console.log(enemyNames[2]);
+//console.log(enemyNames.length);
+
+
+
+var fight = function(enemyNames) {
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
     console.log(promptFight);
     if (promptFight === "fight" || promptFight === "FIGHT") {
         enemyHealth = enemyHealth - playerAttack;
     console.log(
-        playerName + " attacked " + enemyName + "." + enemyName + " now has " + enemyHealth + " health remaining."
+        playerName + " attacked " + enemyNames + "." + enemyNames + " now has " + enemyHealth + " health remaining."
     );
     if (enemyHealth <= 0) {
-        window.alert(enemyName + " has died!");
+        window.alert(enemyNames + " has died!");
     }
     else {
-        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        window.alert(enemyNames + " still has " + enemyHealth + " health left.");
     }
     
     playerHealth = playerHealth - enemyAttack;
     console.log(
-        enemyName + " attacked " + playerName + "." + playerName + " now has " + playerHealth + " health remaining."
+        enemyNames + " attacked " + playerName + "." + playerName + " now has " + playerHealth + " health remaining."
     );
 
     if (playerHealth <= 0) {
@@ -52,7 +71,13 @@ else {
 }
 
 };
+for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames[i]);
+    console.log(i);
+    console.log(enemyNames[i] + " is at " + i + " index ");
+}
 
 
-
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
