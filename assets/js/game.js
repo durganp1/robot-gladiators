972f6aa4d65bc0292975f7796796e2a78881e17b
durganp1 <durganp1@gmail.com>
2020-12-09 12:00:21 -0600

    added random attack values

--- assets/js/game.js
+++ assets/js/game.js
@@ -20,13 +20,14 @@ var fight = function(enemyName) {
 
             if (confirmSkip) {
                 window.alert(playerName + ' has chosen to skip the fight!');
-                playerMoney = playerMoney - 10;
+                playerMoney = Math.max(0, playerMoney - 10);
                 console.log('player money is now ' + playerMoney);
                 break; 
             } 
         }
 
-            enemyHealth = enemyHealth - playerAttack;
+            var damage = randomNumber(playerAttack - 3, playerAttack);
+            enemyHealth = Math.max(0, enemyHealth - damage);
             console.log(playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.');
         
             if (enemyHealth <= 0) {
@@ -38,7 +39,8 @@ var fight = function(enemyName) {
                 window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
             }
 
-            playerHealth = playerHealth - enemyAttack;
+            var damage = randomNumber(enemyAttack - 3, enemyAttack);
+            playerHealth = Math.max(0, playerHealth - damage);
             console.log(enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.');
 
             if (playerHealth <= 0) {
@@ -61,7 +63,7 @@ var startGame = function() {
             window.alert('Welcome to Robot Gladiators!  Round ' + (i + 1));
 
         var pickedEnemyName = enemyNames[i];
-        enemyHealth = 50;
+        enemyHealth = randomNumber(40, 60);
         fight(pickedEnemyName);
             if (playerHealth > 0 && i < enemyNames.length -1) {
                 var storeConfirm = window.confirm('The fight is over, visit the store before the next round?');
@@ -128,6 +130,12 @@ var shop = function() {
             shop();
             break;
     }
+};
+
+var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return value;
 };
 
 startGame();
